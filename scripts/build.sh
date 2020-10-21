188f2626f58c9bdadbf00467fbb183d5a3035985
Andrew Tongen <atongen@gmail.com>
2020-10-21 17:32:38 -0500

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,10 +7,6 @@ name="opixelserv"
 branch="dynamic-tls"
 compiler="4.10.1"
 
-#########
-# ocaml #
-#########
-
 opam update
 
 if ! opam switch list | cat | grep -q "$name"; then
@@ -24,71 +20,84 @@ fi
 
 set -e
 
-########
-# base #
-########
-
-echo "Installing opam base dependencies"
 opam install -yq \
   dune depext conf-libev merlin utop ocp-indent ocp-index \
-
-#################
-# mirage-crypto #
-#################
-
-echo "Installing opam mirage-crypto dependencies"
-opam install -yq \
+  ocaml-migrate-parsetree.1.7.3 \
+  angstrom.0.15.0 \
+  asn1-combinators.0.2.3 \
+  astring.0.8.5 \
+  async.v0.14.0 \
+  base64.3.4.0 \
+  bigarray-compat.1.0.0 \
+  bigstringaf.0.6.1 \
+  cmdliner.1.0.4 \
+  cppo.1.6.6 \
+  csexp.1.3.2 \
   cstruct-sexp.5.2.0 \
   cstruct.5.2.0 \
+  dns-client.4.6.2 \
+  domain-name.0.3.0 \
   duration.0.1.3 \
   eqaf.0.7 \
+  fiat-p256.0.2.1 \
+  fieldslib.v0.14.0 \
+  fmt.0.8.9 \
+  gmap.0.3.0 \
+  hacl_x25519.0.2.0 \
+  hex.1.4.0 \
   hkdf.1.0.4 \
+  ipaddr-sexp.5.0.1 \
+  ipaddr.5.0.1 \
+  js_of_ocaml-ppx.3.7.1 \
+  js_of_ocaml.3.7.1 \
+  jsonm.1.0.1 \
   logs.0.7.0 \
+  lru.0.3.0 \
+  lwt.5.3.0 \
+  macaddr.5.0.1 \
+  magic-mime.1.1.2 \
   mirage-channel.4.0.1 \
   mirage-clock.3.0.1 \
+  mirage-crypto-pk.0.8.5 \
+  mirage-crypto-rng.0.8.5 \
+  mirage-crypto.0.8.5 \
+  mirage-flow-combinators.2.0.1 \
   mirage-flow.2.0.1 \
   mirage-kv.3.0.1 \
-  mirage-runtime.3.8.1 \
+  mirage-no-solo5.1 \
+  mirage-no-xen.1 \
+  mirage-random.2.0.0 \
+  mirage-stack.2.2.0 \
   mirage-time.2.0.1 \
+  mmap.1.1.0 \
   mtime.1.2.0 \
-  ocaml-migrate-parsetree.1.7.3 \
+  num.1.3 \
+  ocplib-endian.1.1 \
+  parsexp.v0.14.0 \
+  ppx_cstruct.5.2.0 \
+  ppx_derivers.1.2.1 \
+  ppx_fields_conv.v0.14.1 \
+  ppx_here.v0.14.0 \
   ppx_sexp_conv.v0.14.1 \
   ppxlib.0.15.0 \
+  psq.0.2.0 \
+  ptime.0.8.5 \
+  re.1.9.0 \
+  result.1.5 \
   rresult.0.6.0 \
   sexplib.v0.14.0 \
+  sexplib0.v0.14.0 \
+  stdlib-shims.0.1.0 \
+  stringext.1.6.0 \
+  topkg.1.0.3 \
+  uchar.0.0.2 \
+  uri-sexp.4.0.0 \
+  uri.4.0.0 \
+  uutf.1.0.2 \
+  vchan.5.0.0 \
   x509.0.11.2 \
+  xenstore.2.1.1 \
   zarith.1.10
-
-cd "${dir}/.." || exit 1
-if [[ ! -d "mirage-crypto" ]]; then
-  git clone https://github.com/mirage/mirage-crypto.git
-fi
-cd mirage-crypto
-
-if [[ "$(git rev-parse --abbrev-ref HEAD)" != "v0.8.5-local" ]]; then
-  git checkout -b v0.8.5-local v0.8.5
-fi
-
-echo "Installing mirage-crypto"
-opam pin add -yn mirage-crypto .
-opam pin add -yn mirage-crypto-pk .
-opam pin add -yn mirage-crypto-rng .
-dune clean
-dune build
-dune install >/dev/null
-
-#############
-# ocaml-tls #
-#############
-
-echo "Installing opam ocaml-tls dependencies"
-opam install -yq \
-  domain-name.0.3.0 \
-  fiat-p256.0.2.1 \
-  fmt.0.8.9 \
-  hacl_x25519.0.2.0 \
-  ppx_cstruct.5.2.0 \
-  ptime.0.8.5 \
 
 cd "${dir}/.." || exit 1
 if [[ ! -d "ocaml-tls" ]]; then
@@ -102,29 +111,10 @@ fi
 
 git pull --ff-only
 
-echo "Installing ocaml-tls"
 opam pin add -yn .
 dune clean
 dune build
 dune install >/dev/null
-
-#################
-# ocaml-conduit #
-#################
-
-echo "Installing opam ocaml-condiut dependencies"
-opam install -yq \
-  async.v0.14.0 \
-  dns-client.4.6.2 \
-  ipaddr.5.0.1 \
-  ipaddr-sexp.5.0.1 \
-  mirage-flow-combinators.2.0.1 \
-  mirage-random.2.0.0 \
-  mirage-stack.2.2.0 \
-  ppx_here.v0.14.0 \
-  uri.3.1.0 \
-  vchan.5.0.0 \
-  xenstore.2.1.1 \
 
 cd "${dir}/.." || exit 1
 if [[ ! -d "ocaml-conduit" ]]; then
@@ -138,25 +128,12 @@ fi
 
 git pull --ff-only
 
-echo "Installing ocaml-condiut"
 opam pin add -yn conduit .
 opam pin add -yn conduit-lwt .
 opam pin add -yn conduit-lwt-unix .
 dune clean
 dune build
 dune install >/dev/null
-
-################
-# ocaml-cohttp #
-################
-
-echo "Installing opam ocaml-cohttp dependencies"
-opam install -yq \
-  js_of_ocaml.3.7.1 \
-  js_of_ocaml-ppx.3.7.1 \
-  jsonm.1.0.1 \
-  magic-mime.1.1.2 \
-  uri-sexp.3.1.0
 
 cd "${dir}/.." || exit 1
 if [[ ! -d "ocaml-cohttp" ]]; then
@@ -168,7 +145,6 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != "v2.5.4-local" ]]; then
   git checkout -b v2.5.4-local v2.5.4
 fi
 
-echo "Installing ocaml-cohttp"
 opam pin add -yn cohttp .
 opam pin add -yn cohttp-lwt .
 opam pin add -yn cohttp-lwt-unix .
@@ -176,11 +152,6 @@ dune clean
 dune build
 dune install >/dev/null
 
-##############
-# opixelserv #
-##############
-
-echo "Installing opixelserv"
 cd "${dir}" || exit 1
 opam pin add -yn opixelserv .
 dune clean
