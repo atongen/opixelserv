66bd240d1d1f4818ff3b3de75bb99f738325c68c
Andrew Tongen <atongen@gmail.com>
2020-11-15 17:20:40 -0600

    Remove obsolte pixelserv-tls test dir

--- test/req.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# assumes www.nice.org resolves to localhost and/or pixelserve-tls
-host="${1:-www.nice.org}"
-
-curl \
-  -I \
-  --cacert var/cache/pixelserv/ca.crt \
-  "https://${host}/index.html"
--- test/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  --rm \
-  --name pixelserv-tls \
-  --net=host \
-  -v `pwd`/var/cache/pixelserv:/var/cache/pixelserv \
-  imthai/pixelserv-tls
