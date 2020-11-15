5b5062b113e2b7cfecacd333c36f0cd04dcb6c56
Andrew Tongen <atongen@gmail.com>
2020-11-15 12:07:58 -0600

    Moar bsd linux sed compat

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -10,6 +10,7 @@ git checkout "$info_file"
 
 function finish {
   git checkout "$info_file"
+  rm -f "${info_file}.bak"
 }
 trap finish EXIT
 
@@ -21,13 +22,13 @@ ocaml_version=$(ocaml -vnum)
 tls_hash=$(cd "${dir}/../ocaml-tls" && git rev-parse HEAD | cut -c 1-7 2>/dev/null || echo "unset")
 conduit_hash=$(cd "${dir}/../ocaml-conduit" && git rev-parse HEAD | cut -c 1-7 2>/dev/null || echo "unset")
 
-sed -i 's/version = "[^"]*"/version = "'"${version}"'"/' "$info_file"
-sed -i 's/build_time = "[^"]*"/build_time = "'"${build_time}"'"/' "$info_file"
-sed -i 's/build_hash = "[^"]*"/build_hash = "'"${build_hash}"'"/' "$info_file"
-sed -i 's/ocaml_version = "[^"]*"/ocaml_version = "'"${ocaml_version}"'"/' "$info_file"
-sed -i 's|bug_reports = "[^"]*"|bug_reports = "'"${bug_reports}"'"|' "$info_file"
-sed -i 's/tls_hash = "[^"]*"/tls_hash = "'"${tls_hash}"'"/' "$info_file"
-sed -i 's/conduit_hash = "[^"]*"/conduit_hash = "'"${conduit_hash}"'"/' "$info_file"
+sed -i.bak 's/version = "[^"]*"/version = "'"${version}"'"/' "$info_file"
+sed -i.bak 's/build_time = "[^"]*"/build_time = "'"${build_time}"'"/' "$info_file"
+sed -i.bak 's/build_hash = "[^"]*"/build_hash = "'"${build_hash}"'"/' "$info_file"
+sed -i.bak 's/ocaml_version = "[^"]*"/ocaml_version = "'"${ocaml_version}"'"/' "$info_file"
+sed -i.bak 's|bug_reports = "[^"]*"|bug_reports = "'"${bug_reports}"'"|' "$info_file"
+sed -i.bak 's/tls_hash = "[^"]*"/tls_hash = "'"${tls_hash}"'"/' "$info_file"
+sed -i.bak 's/conduit_hash = "[^"]*"/conduit_hash = "'"${conduit_hash}"'"/' "$info_file"
 
 dune build @install
 test -L bin || ln -s _build/install/default/bin .
